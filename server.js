abd8b4b07e174027be598de33e5d9e8c4ebc652f
hkarthi10 <karthimenaka1028@gmail.com>
2025-03-10 05:45:00 +0530

    Update server.js issue

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ mongoose.connect(mongourl)
     .then(() => {
         console.log('Connected to database')
         app.listen(PORT, () => {
-            console.log('Server is running on port 3000.')
+            console.log(`Server is running on port ${PORT}.`)
+
         })
     })
     .catch((error) => {
